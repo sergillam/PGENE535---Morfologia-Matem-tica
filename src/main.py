06696f00e275b74fc114fc67f8a488b81c1ad176
unknown <sgm.oliveira96@gmail.com>
2020-12-10 10:56:40 -0400

    miniprojeto02 - Adicionado arquivo de saida com o texto que foi conseguido interpretar da imagem

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import cv2 
 import numpy as np 
 
@@ -11,8 +10,19 @@ import pytesseract
 path_tesseract = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
 pytesseract.pytesseract.tesseract_cmd = path_tesseract
 
-kernel = np.ones((11,11), np.uint8) 
-iteration = 1000
+kernel = np.ones((31,31), np.uint8) 
+iteration = 5000
+amount_images = 5
+
+
+def saveResult(name_file, result, id):
+    try:
+        file = open(name_file, 'a+', encoding="utf-8")
+    except FileNotFoundError:
+        file = open(name_file, 'w+', encoding="utf-8")
+    str_result = str(id) + ' ' + result + '\n' 
+    file.writelines(str_result)
+    file.close()
 
 def addZero(idx):
     if idx < 10:
@@ -44,6 +54,15 @@ def dilationOperation(image_gray):
     image_result = cv2.subtract(image_dilation, image_gray) 
     return image_result
 
+def saveImageGray(image_gray, idx): 
+    path_save = r'../image_gray/'
+    image_extesion = '.jpg'
+    image_name = 'image_' + addZero(idx) + str(idx) + image_extesion
+    image_url = path_save + image_name
+    
+    cv2.imwrite(image_url, image_gray)
+    saveValueBoard(path_save,idx)
+    
 def saveImageErosion(image_gray, idx): 
     path_save = r'../image_erosion/'
     image_extesion = '.jpg'
@@ -64,22 +83,23 @@ def saveImageDilation(image_gray, idx):
     image_dilation = dilationOperation(image_gray)
     
     cv2.imwrite(image_url, image_dilation)
+    saveValueBoard(path_save,idx)
     
-
-
 def saveValueBoard(path, idx):
     path_save = path
     image_extesion = '.jpg'
     image_name = 'image_' + addZero(idx) + str(idx) + image_extesion
     image_url = path_save + image_name
     
-    
-    print(pytesseract.image_to_string(Image.open(image_url)))
+    result_text = pytesseract.image_to_string(Image.open(image_url))
+    result_path = path + 'result.txt'
+    saveResult(result_path, result_text, idx)
 
-for idx in range(1,5,1):
+for idx in range(1,amount_images,1):
+    print(idx)
     image_gray = loadImage(idx)
-        
+    
+    saveImageGray(image_gray,idx)   
     saveImageDilation(image_gray,idx)
     saveImageErosion(image_gray, idx)
-    
     
